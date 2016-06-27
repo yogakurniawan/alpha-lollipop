e67aba08f372d0061c0a4da9c9c67dc7143ba9d6
yogakurniawan <yoga.kwn@gmail.com>
2016-06-27 16:26:08 +0700

    fix(user) to return complete user data after login

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,9 @@
 var config = require('../../server/config.json');
 var path = require('path');
 
-module.exports = function(user) {
+module.exports = function (user) {
     //send verification email after registration
-    user.afterRemote('create', function(context, user, next) {
+    user.afterRemote('create', function (context, user, next) {
         console.log('> user.afterRemote triggered');
 
         var options = {
@@ -17,7 +17,7 @@ module.exports = function(user) {
             user: user
         };
 
-        user.verify(options, function(err, response) {
+        user.verify(options, function (err, response) {
             if (err) return next(err);
 
             console.log('> verification email sent:', response);
@@ -26,7 +26,7 @@ module.exports = function(user) {
     });
 
     //send password reset link when requested
-    user.on('resetPasswordRequest', function(info) {
+    user.on('resetPasswordRequest', function (info) {
         var url = 'http://' + config.host + ':' + config.port + '/reset-password';
         var html = 'Click <a href="' + url + '?access_token=' +
             info.accessToken.id + '">here</a> to reset your password';
@@ -36,9 +36,19 @@ module.exports = function(user) {
             from: info.email,
             subject: 'Password reset',
             html: html
-        }, function(err) {
+        }, function (err) {
             if (err) return console.log('> error sending password reset email');
             console.log('> sending password reset email to:', info.email);
         });
+    });
+
+    user.afterRemote('login', function (ctx, userInstance, next) {
+        let userId = userInstance.userId.toString();
+        let userDb = user.findById(userId, function (err, userObject) {
+            if (!err) {
+                ctx.result = userObject;
+                next();
+            }
+        });        
     });
 };
