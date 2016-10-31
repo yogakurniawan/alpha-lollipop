1d2f4b7c754d76fa0b878173576cc54919682afb
yogakurniawan <yoga.kwn@gmail.com>
2016-10-31 11:06:10 +0700

    Remove api key from public repo

--- server/boot/email-setup.js
+++ server/boot/email-setup.js
@@ -1,6 +1,9 @@
 /**
  * This file is responsible for setting up the Email plugin if any.
  */
+
+var loopback = require('loopback');
+
 module.exports = function (app) {
   if (!process.env.SENDGRID_API_KEY) return;
   var DataSource = require('loopback-datasource-juggler').DataSource;
