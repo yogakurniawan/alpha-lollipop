c9d2c4854540e2b40156a760823d8400867d3336
yogakurniawan <yoga.kwn@gmail.com>
2016-11-01 16:57:49 +0700

    fix sendgrid email datasource

--- server/models/user.js
+++ server/models/user.js
@@ -25,7 +25,6 @@ module.exports = function (user) {
             console.log('> verification email sent:', response);
             context.res.send(response);
         });
-        context.res.send({"success": true});
     });
 
     //send password reset link when requested
