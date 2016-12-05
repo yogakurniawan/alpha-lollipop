e92526ed56504a0e99c7b5e26519e0823c7f5954
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-12-05 21:11:54 +0700

    change api url

--- server/models/user.js
+++ server/models/user.js
@@ -13,7 +13,7 @@ module.exports = function (user) {
             from: 'noreply@loopback.com',
             subject: 'Thanks for registering.',
             text: "{href}",
-            host: "http://54.254.154.139:9000",
+            host: "54.254.154.139:9000",
             template: path.resolve(__dirname, '../../server/views/verify.ejs'),
             redirect: 'https://cerebral-app.herokuapp.com/emailVerified',
             user: user
